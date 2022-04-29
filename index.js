1567958b6cfd8686478bc2a72609442c159dbff2
caiden nason <caidenbnason@gmail.com>
2022-04-29 10:27:51 -0400

    commit tree 4bd8459dbff9ccbcc99d225800083cdaea4990b0

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record){
+  const scores = record.find(element => element.result ==='W')
+        if (scores === undefined){
+            return undefined
+        } else {
+            return scores.year
+        }
+}
+
+
+//record.find(element => element.result === 'W')
+
